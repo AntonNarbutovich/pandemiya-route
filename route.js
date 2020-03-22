60490d8d76bbe521d97de097f8b873e5755896b9
anarbutovich <dezend2@gmail.com>
2020-03-22 13:13:42 +0300

    changed query

--- route.js
+++ route.js
@@ -6,22 +6,18 @@ mongoose.connect(db.url, {useNewUrlParser: true, useUnifiedTopology: true});
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-	name: String,
-	english: String,
 	alpha3: String,
-	iso: String
+	state: String,
+	closed_list: String,
 });
 
-var countries = mongoose.model('countries', schema);
+var countries = mongoose.model('opened_borders', schema);
 
 module.exports = function(app) {
     app.get('/', (req, res) => {
 	const { country } = req.query
-	const alpha = {'alpha3' : 'BLR'}
 	
-
-
-	const result = countries.find({'alpha3' : 'BLR'}, "name", function(err, data){
+	const result = countries.find({ $or:[ {'state':'closed'}, {$and:[{'state':'partially closed'}, {'closed_list':country}]}]}, {_id:0}).lean().exec( function(err, data){
 			console.log(">>>> " + data );
 			res.send(JSON.stringify(data))
 		});
